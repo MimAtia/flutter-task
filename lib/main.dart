cf028ec8300f691157223eb42f9050d4bb8474c8
MimAtia <mim15-5375@diu.edu.bd>
2025-04-11 12:53:23 +0600

    task 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'tasks/task1_hello_world.dart';
+import 'tasks/task2_button_press.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +10,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Task 1',
+      title: 'Flutter Tasks',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Task1HelloWorld(),
+      home: TaskSelectionScreen(),
+    );
+  }
+}
+
+class TaskSelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Tasks'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Task 1: Hello World'),
+            subtitle: Text('Display Hello World with custom styling'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Task1HelloWorld()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Task 2: Button Press'),
+            subtitle: Text('Change text on button press'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Task2ButtonPress()),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/tasks/task2_button_press.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Task2ButtonPress extends StatefulWidget {
+  @override
+  _Task2ButtonPressState createState() => _Task2ButtonPressState();
+}
+
+class _Task2ButtonPressState extends State<Task2ButtonPress> {
+  String displayText = "Press the button";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task 2: Button Press'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              displayText,
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  displayText = "Button Pressed!";
+                });
+              },
+              child: Text('Press Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+} 
